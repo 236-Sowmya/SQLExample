45c718026a97aed80cac3a86448a2d3259abedd1
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-11 15:21:16 +0530

    Add project files.

---
+++ SQLExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(@"data source=(localdb)\MSSQLLocalDB;database=NorthWind; integrated security=SSPI");
+                SqlCommand cm = new SqlCommand("select * from customers", con);
+                con.Open();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["CompanyName"] + " " + sdr["Region"]);
+                }
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something is wrong" + e);
+            }
+        }
+    }
+}
